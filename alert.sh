1d5469b7221cf52c15e73112417e3c1487e135c1
Pavel Vaysberg <pavel@draft.com>
2018-03-02 13:59:27 -0500

    Replace underscore with dashes in pipeline

--- alert.sh
+++ alert.sh
@@ -1,4 +1,4 @@
-FAVORITE_COLOR=$(buildkite-agent meta-data get favorite_color)
-USER_TO_ALERT=$(buildkite-agent meta-data get user_to_alert)
+FAVORITE_COLOR=$(buildkite-agent meta-data get favorite-color)
+USER_TO_ALERT=$(buildkite-agent meta-data get user-to-alert)
 
 echo "I have alerted $USER_TO_ALERT that your favorite color is $FAVORITE_COLOR"
